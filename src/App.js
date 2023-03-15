140c7eb2d6aa00a5d8661c3e45c576c79c47a20a
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-03-15 21:01:20 +0200

    Add skeleton html and scss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 const App = () => {
   return (
-    <h1>
-      <p>Weather Radar</p>
-    </h1>
+    <>
+      <div className='header-container'>
+        <p>Säätutka</p>
+      </div>
+      <div className='container'>
+        <div className='selector-container'>
+          <select name='office' id='office'>
+            <option value='all'>Kaikki kaupungit</option>
+            <option value='tampere'>Tampere</option>
+            <option value='jyvaskyla'>Jyväskylä</option>
+            <option value='kuopio'>Kuopio</option>
+            <option value='espoo'>Espoo</option>
+          </select>
+        </div>
+        <div className='report-container'>
+          <div className='card'>
+            <div className=''>
+              <p>Espoo</p>
+              <p>Scattered clouds</p>
+            </div>
+          </div>
+          <div className='trihourly'>
+            <div className='trihourly-item'>15:00</div>
+            <div className='trihourly-item'>15:00</div>
+            <div className='trihourly-item'>15:00</div>
+            <div className='trihourly-item'>15:00</div>
+            <div className='trihourly-item'>15:00</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default App;
