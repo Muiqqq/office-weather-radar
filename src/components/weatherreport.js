ec9e82beb77f1d19e0b4eb544f4b2d35a9be9caf
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-03-16 14:46:48 +0200

    Refine css and html for WeatherReport component

--- src/components/weatherreport.js
+++ src/components/weatherreport.js
@@ -15,9 +15,30 @@ const Trihourly = () => {
 const Card = () => {
   return (
     <div className='card'>
-      <div className=''>
-        <p>Espoo</p>
-        <p>Scattered clouds</p>
+      <div className='card-row'>
+        <div className='card-item align-center'>
+          <p className='city'>Espoo</p>
+          <p className='secondary-text'>Scattered clouds</p>
+        </div>
+        <div className='card-item flex primary-temperature'>
+          <img
+            className='card-weather-icon'
+            src='https://openweathermap.org/img/wn/10d@2x.png'
+            alt='Icon for current weather'
+          ></img>
+          <p className='align-center'>13 C</p>
+        </div>
+      </div>
+      <div className='card-row'>
+        <div className='card-item align-end'>
+          <p className='date'>May 2nd</p>
+          <p className='secondary-text'>12:00</p>
+        </div>
+        <div className='card-item'>
+          <p className='secondary-text'>Wind: 0.00 m/s</p>
+          <p className='secondary-text'>Humidity: 99 %</p>
+          <p className='secondary-text'>Precipitation (3h): 0 mm</p>
+        </div>
       </div>
     </div>
   );
@@ -27,7 +48,6 @@ const WeatherReport = ({}) => {
   return (
     <div className='report-container'>
       <Card />
-      <Trihourly />
     </div>
   );
 };
