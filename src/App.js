8abe467bd4823a448a8ea846721ef53a04c2f843
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-03-16 21:08:35 +0200

    Use WeatherReportList in App
    Add coordinates to listOptions
    note to self: rename listOptions to something more relevant and
    move it elsewhere.
    Add handleSelection(), based on currently selected item,
    constructs a list of locations to fetch the weather for.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Selector from './components/selector';
-import WeatherReport from './components/weatherreport';
+import WeatherReportList from './components/weatherReportList';
 
 const listOptions = [
   { value: 'all', description: 'Kaikki kaupungit' },
   {
     value: 'tampere',
     description: 'Tampere',
+    lat: 61.4991,
+    lon: 23.7871,
   },
   {
     value: 'jyvaskyla',
     description: 'Jyväskylä',
+    lat: 62.2415,
+    lon: 25.7209,
   },
   {
     value: 'kuopio',
     description: 'Kuopio',
+    lat: 62.8924,
+    lon: 27.677,
   },
   {
     value: 'espoo',
     description: 'Espoo',
+    lat: 60.25,
+    lon: 24.6667,
   },
 ];
 
 const App = () => {
   const [selected, setSelected] = useState(listOptions[0].value);
-  console.log(selected);
+
+  const handleSelection = () => {
+    if (selected === listOptions[0].value) {
+      return listOptions.slice(1);
+    } else {
+      return listOptions.filter((elem) => elem.value === selected);
+    }
+  };
 
   const onSelect = (event) => {
     setSelected(event.target.value);
@@ -44,7 +59,7 @@ const App = () => {
           options={listOptions}
           onChange={onSelect}
         />
-        <WeatherReport />
+        <WeatherReportList listOfFetchables={handleSelection()} />
       </div>
     </>
   );
