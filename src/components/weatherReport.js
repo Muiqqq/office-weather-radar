3440681f331976b89be4605764dcc1f0171e916f
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-03-19 18:46:44 +0200

    Move urls down to WeatherReport and pass lat and lon to it instead

--- src/components/weatherReport.js
+++ src/components/weatherReport.js
@@ -3,11 +3,26 @@ import axios from 'axios';
 import Forecast from './forecast';
 import WeatherCard from './weatherCard';
 
-const WeatherReport = ({ currentWeatherURL, forecastURL }) => {
+// Should not have secrets anywhere near a React app as they are visible in
+// the build! Proper way might be to make our own backend API which would store
+// the secrets and handle the actual fetching from OpenWeatherMap, and use
+// it as a proxy.
+const APIKEY = `${process.env.REACT_APP_API_KEY}`;
+
+const options = {
+  lang: 'en',
+  units: 'metric',
+  count: 5,
+};
+
+const WeatherReport = ({ lat, lon }) => {
   const [currentWeatherData, setCurrentWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
+
+  const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=${options.units}&lang=${options.lang}`;
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=${options.units}&lang=${options.lang}&cnt=${options.count}`;
 
   useEffect(() => {
     const fetchWeather = async () => {
