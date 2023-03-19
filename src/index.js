e70d95ef0fef6ab22753614cbc18005616e26576
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-03-19 19:31:26 +0200

    Move style.scss under /styles folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.scss';
+import './styles/style.scss';
 import './styles/loading-spinner.scss';
 import reportWebVitals from './reportWebVitals';
 
