9633c47ef7b8bd8b9610ce22c47894bc781c9d75
Aleksi Muikku <aleksi.muikku@hotmail.com>
2023-04-05 10:17:04 +0300

    Add entry file for the backend server, server.js

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/route.js');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
